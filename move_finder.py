2332798b557180fcfcd8238457d21fda8db1a2b1
Jonathan Perlman <31137025+jonman5@users.noreply.github.com>
2022-11-16 18:42:47 -0500

    Add ability to find all moves including left and right

--- move_finder.py
+++ move_finder.py
@@ -22,20 +22,20 @@ class MoveFinder(object):
                         move_up = cls.__test_move_up(gameboard, col, row_index)
                         if move_up is not None:
                             moves.append(move_up)
-                    # if col != 0:
-                    #     move_right = cls.__test_move_right(gameboard, row, col)
-                    #     if move_right is not None:
-                    #         moves.append(move_right)
-                    # if col != 5:
-                    #     move_left = cls.__test_move_left(gameboard, col)
-                    #     if move_left is not None:
-                    #         moves.append(move_left)
+                    if col != 0:
+                        move_right = cls.__test_move_right(gameboard, col, row_index)
+                        if move_right is not None:
+                            moves.append(move_right)
+                    if col != 5:
+                        move_left = cls.__test_move_left(gameboard, col, row_index)
+                        if move_left is not None:
+                            moves.append(move_left)
         return moves
 
     @classmethod
     # Check if the vehicle above spot(x, y) can move down, if so return that move
     def __test_move_down(cls, gameboard: GameBoard, x, y):
-        # Find vehicle directly above spot of interest and return Move which moves it down to spot of interest
+        # Find adjacent vehicle above spot of interest and return Move which moves it down to spot of interest
         spot_above = gameboard.get_spot_at(x, y - 1)
         if spot_above.isalpha():
             vehicle_above: Vehicle = gameboard.vehicles.get(spot_above)
@@ -44,7 +44,7 @@ class MoveFinder(object):
                 return Move(vehicle_above.name, Direction.DOWN, 1, heuristic)
             else:
                 return None
-        # If spot above is not a vehicle:
+        # If adjacent spot above is not a vehicle:
         # Find the closest vehicle above spot of interest and return Move which moves it down to spot of interest
         free_spots_above = 0
         while spot_above == '.' and (y - free_spots_above - 2) >= 0:
@@ -59,7 +59,7 @@ class MoveFinder(object):
     @classmethod
     # Check if the vehicle below spot(x, y) can move up, if so return that move
     def __test_move_up(cls, gameboard: GameBoard, x, y):
-        # Find vehicle directly below spot of interest and return Move which moves it up to spot of interest
+        # Find adjacent vehicle below spot of interest and return Move which moves it up to spot of interest
         spot_below = gameboard.get_spot_at(x, y + 1)
         if spot_below.isalpha():
             vehicle_below: Vehicle = gameboard.vehicles.get(spot_below)
@@ -69,7 +69,7 @@ class MoveFinder(object):
             else:
                 return None
 
-        # If spot below is not a vehicle:
+        # If adjacent spot below is not a vehicle:
         # Find the closest vehicle below spot of interest and return Move which moves it up to spot of interest
         free_spots_below = 0
         while spot_below == '.' and (y + free_spots_below + 2) < 6:
@@ -80,3 +80,52 @@ class MoveFinder(object):
             if vehicle_below.get_orientation() == Orientation.VERTICAL and vehicle_below.fuel_level > free_spots_below:
                 heuristic = 1
                 return Move(vehicle_below.name, Direction.UP, free_spots_below+1, heuristic)
+
+    @classmethod
+    # Check if the adjacent vehicle left of spot(x, y) can move right, if so return that move
+    def __test_move_right(cls, gameboard: GameBoard, x, y):
+        # Find adjacent vehicle left of spot of interest and return Move which moves it right to spot of interest
+        spot_above = gameboard.get_spot_at(x - 1, y)
+        if spot_above.isalpha():
+            vehicle_above: Vehicle = gameboard.vehicles.get(spot_above)
+            if vehicle_above.get_orientation() == Orientation.HORIZONTAL and vehicle_above.fuel_level > 0:
+                heuristic = 1
+                return Move(vehicle_above.name, Direction.RIGHT, 1, heuristic)
+            else:
+                return None
+        # If adjacent spot to left is not a vehicle:
+        # Find the closest vehicle left of spot of interest and return Move which moves it right to spot of interest
+        free_spots_to_left = 0
+        while spot_above == '.' and (x - free_spots_to_left - 2) >= 0:
+            free_spots_to_left += 1
+            spot_above = gameboard.get_spot_at(x - free_spots_to_left - 1, y)
+        if spot_above.isalpha():
+            vehicle_above: Vehicle = gameboard.vehicles.get(spot_above)
+            if vehicle_above.get_orientation() == Orientation.HORIZONTAL and vehicle_above.fuel_level > free_spots_to_left:
+                heuristic = 1
+                return Move(vehicle_above.name, Direction.RIGHT, free_spots_to_left + 1, heuristic)
+
+    @classmethod
+    # Check if the adjacent vehicle right of spot(x, y) can move left, if so return that move
+    def __test_move_left(cls, gameboard: GameBoard, x, y):
+        # Find adjacent vehicle right of spot of interest and return Move which moves it left to spot of interest
+        spot_below = gameboard.get_spot_at(x + 1, y)
+        if spot_below.isalpha():
+            vehicle_below: Vehicle = gameboard.vehicles.get(spot_below)
+            if vehicle_below.get_orientation() == Orientation.HORIZONTAL and vehicle_below.fuel_level > 0:
+                heuristic = 1
+                return Move(vehicle_below.name, Direction.LEFT, 1, heuristic)
+            else:
+                return None
+
+        # If spot to right is not a vehicle:
+        # Find the closest vehicle right of spot of interest and return Move which moves it left to spot of interest
+        free_spots_to_right = 0
+        while spot_below == '.' and (x + free_spots_to_right + 2) < 6:
+            free_spots_to_right += 1
+            spot_below = gameboard.get_spot_at(x + free_spots_to_right + 1, y)
+        if spot_below.isalpha():
+            vehicle_below: Vehicle = gameboard.vehicles.get(spot_below)
+            if vehicle_below.get_orientation() == Orientation.HORIZONTAL and vehicle_below.fuel_level > free_spots_to_right:
+                heuristic = 1
+                return Move(vehicle_below.name, Direction.LEFT, free_spots_to_right+1, heuristic)
