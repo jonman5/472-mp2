b7147b40f8fc8a05f5ffca052d3b16427bdc8cc2
Jonathan Perlman <31137025+jonman5@users.noreply.github.com>
2022-11-21 12:13:22 -0500

    Add heuristic helper and GBFS search algorithm

---
+++ helper/heuristic_helper.py
@@ -0,0 +1,14 @@
+from model.node import Node
+
+
+def calculate_heuristic(heuristic_to_use, node: Node):
+    match heuristic_to_use:
+        case 'h1':
+            pass
+        case 'h2':
+            pass
+        case 'h3':
+            pass
+        case 'h4':
+            pass
+    return 0
--- node_searcher.py
+++ node_searcher.py
@@ -1,4 +1,6 @@
 from typing import Type
+
+from helper.heuristic_helper import *
 from model.node import Node
 from model.prioritized_node import PrioritizedNode
 from move_finder import MoveFinder
@@ -14,10 +16,17 @@ class NodeSearcher(object):
         self.heuristic_used = heuristic_to_use
         self.algorithm = algo
 
-    def execute_search(self, initial_node: Node):
+    def execute_search(self, initial_node: Node, heuristic_to_use=None):
         match self.algorithm:
             case "UCS":
+                self.open_list = PriorityQueue()
                 return self.__uniform_cost_search(initial_node)
+            case "GBFS":
+                self.open_list = PriorityQueue()
+                return self.__greedy_best_first_search(initial_node, heuristic_to_use)
+            case "A/A*":
+                self.open_list = PriorityQueue()
+                pass
 
     def __uniform_cost_search(self, initial_state: Node) -> Node:
         self.open_list.put(PrioritizedNode(initial_state.get_depth(), initial_state))
@@ -47,6 +56,28 @@ class NodeSearcher(object):
                                 break
                     if not successor_inserted:
                         self.open_list.put(PrioritizedNode(successor.get_depth(), successor))
+        return None
+
+    def __greedy_best_first_search(self, initial_state: Node, heuristic_to_use) -> Node:
+        self.open_list.put(PrioritizedNode(calculate_heuristic(heuristic_to_use, initial_state), initial_state))
+        self.closed_list = []
+        while not self.open_list.empty():
+            node_to_search: Node = self.open_list.get().node
+            if node_to_search.get_is_goal():
+                return node_to_search
+            self.closed_list.append(node_to_search)
+            possible_moves = MoveFinder.find_moves(node_to_search.get_state())
+            successors = []
+            for move in possible_moves:
+                successors.append(Node(move, node_to_search))
+            for successor in successors:
+                visited = False
+                for node in self.closed_list:
+                    if successor.get_state() == node.get_state():
+                        visited = True
+                        break
+                if not visited:
+                    self.open_list.put(PrioritizedNode(calculate_heuristic(heuristic_to_use, initial_state), successor))
         return None
 
     def __swap_nodes(self, old_node: Node, new_node: Node):
